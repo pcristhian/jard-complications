d002b57dc03d665345f5377b80479fb410b2f9bb
pcristhian <dont6worry@gmail.com>
2026-03-10 09:38:41 -0400

    json fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-<<<<<<< HEAD
-  reactCompiler: true,
-=======
->>>>>>> 08c9acb82890a62e6709e6fab4f08c9ec3d643e6
+  reactCompiler: true
 };
 
 export default nextConfig;
